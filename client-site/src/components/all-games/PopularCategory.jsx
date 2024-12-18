26a078c491042814f1569e6e828b83357482b5b8
vismodeb <vismodeb2000@gmail.com>
2024-12-18 21:57:20 +0600

    mobile device done

--- client-site/src/components/all-games/PopularCategory.jsx
+++ client-site/src/components/all-games/PopularCategory.jsx
@@ -11,6 +11,15 @@ import coinVolcanoImg from "../../assets/popular/8.jpg";
 import luckyNekoImg from "../../assets/popular/9.jpg";
 import bonusManiaImg from "../../assets/popular/10.jpg";
 import sunOfEgyptImg from "../../assets/popular/11.jpg";
+import coinStrikeImg from "../../assets/popular/12.jpg";
+import beachLife from "../../assets/popular/13.jpg";
+import sugarRush from "../../assets/popular/14.jpg";
+import cricketCrash from "../../assets/popular/15.jpg";
+import sweetBonanza from "../../assets/popular/16.jpg";
+import chickyRun from "../../assets/popular/17.jpg";
+import balloon from "../../assets/popular/18.jpg";
+import headsTails from "../../assets/popular/19.jpg";
+import chance from "../../assets/popular/20.jpg";
 import "swiper/css";
 import "swiper/css/grid";
 import "swiper/css/navigation";
@@ -41,15 +50,15 @@ const PopularCategory = () => {
     { id: 9, img: luckyNekoImg, title: "Lucky Neko" },
     { id: 10, img: bonusManiaImg, title: "Bonus Mania" },
     { id: 11, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 12, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 13, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 14, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 15, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 16, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 17, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 18, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 19, img: sunOfEgyptImg, title: "Sun Of Egypt" },
-    { id: 20, img: sunOfEgyptImg, title: "Sun Of Egypt" },
+    { id: 12, img: coinStrikeImg, title: "Coin Strike" },
+    { id: 13, img: beachLife, title: "Beach Life" },
+    { id: 14, img: sugarRush, title: "Sugar Rush" },
+    { id: 15, img: cricketCrash, title: "Cricket Crash" },
+    { id: 16, img: sweetBonanza, title: "Sweet Bonanza" },
+    { id: 17, img: chickyRun, title: "Chicky Run" },
+    { id: 18, img: balloon, title: "Balloon" },
+    { id: 19, img: headsTails, title: "Heads Tails" },
+    { id: 20, img: chance, title: "Chance" },
   ];
 
   return (
